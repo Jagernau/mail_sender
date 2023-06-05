ebf070a134f115aa46a95c5b4147c243af52777b
jagernau <stepanenkov25@gmail.com>
2023-06-05 23:37:39 +0200

    с датами

--- funcs.py
+++ funcs.py
@@ -1,10 +1,14 @@
 import csv
 import pandas as pd
 from styleframe import StyleFrame
+import datetime
+import locale
+
+
 def clients_to_dict(filename):
     data_dict = {}
 
-    with open(filename, newline='') as csvfile:
+    with open(filename, newline='', encoding="utf8") as csvfile:
         reader = csv.reader(csvfile)
         next(reader)  # Пропускаем заголовок файла
         for row in reader:
@@ -16,22 +20,36 @@ def clients_to_dict(filename):
                 data_dict[email] = [values]
 
     return data_dict
-#колонны по максимкльной длине системы мониторинга
-def create_excel_from_list(value):
-    df = pd.DataFrame(value, columns=['Клиет','Система мониторинга', 'Имя объекта', 'Количество дней', 'Цена'])
+
+def create_pdf_from_list(value):
+    df = pd.DataFrame(value, columns=['Клиет', 'Система мониторинга', 'Имя объекта', 'Количество дней', 'Цена'])
     df['Цена'] = df['Цена'].astype(float)
     excel_writer = StyleFrame.ExcelWriter('detalisation.xlsx')
     sf = StyleFrame(df)
-    sf.set_column_width(columns=['Имя объекта',],
-                    width=36)
+    sf.set_column_width(columns=['Имя объекта', ],
+                        width=36)
     sf.set_column_width(columns=['Клиет'], width=15)
     sf.set_column_width(columns=['Количество дней'], width=15)
 
     sf.to_excel(excel_writer=excel_writer)
     excel_writer.save()
-#    workbook = Workbook('output.xlsx')
-#    pdf_options = PdfSaveOptions()
-#    workbook.save('output.pdf', pdf_options)
+
+def get_date_last_month():
+    months = {
+    '1': 'январь',
+    '2': 'февраль',
+    '3': 'март',
+    '4': 'апрель',
+    '5': 'май',
+    '6': 'июнь',
+    '7': 'июль',
+    '8': 'август',
+    '9': 'сентябрь',
+    '10': 'октябрь',
+    '11': 'ноябрь',
+    '12': 'декабрь'
+}
+    return months[str(int(datetime.date.today().month)-1)]
     
-
-
+def get_current_year():
+    return datetime.date.today().year
--- main.py
+++ main.py
@@ -2,27 +2,41 @@ import config
 from jinja2 import Environment, FileSystemLoader
 import classes
 import funcs
+import time
+
+
+last_month = funcs.get_date_last_month()
+current_year = funcs.get_current_year()
 
 smtp_server = config.SMTP_SERVER
 email_sender = config.EMAIL_SENDER
 email_password = config.EMAIL_PASSWORD
+sender_name = config.SENDER_NAME
+sender_phone = config.SENDER_PHONE
 
-dict_clients = funcs.clients_to_dict(filename="./Детализация_для_рассылки.csv")
+
+time.sleep(60)
+dict_clients = funcs.clients_to_dict(filename="./detalisation.csv")
 
 #print(dict_clients)
 
 for key, value in dict_clients.items():
     email_receiver = str(key)
-    email_subject = 'Детализация'
+    email_subject = f'Детализация за {last_month} {current_year} от Сантел Сервис'
     filename = 'detalisation.xlsx'
 
     environment = Environment(loader=FileSystemLoader("templates/"))
-    template = environment.get_template("test.html")
-    content = template.render(key=key, value=value)
-    funcs.create_excel_from_list(value)
+    template = environment.get_template("file_sender.html")
+
+    content = template.render(
+            key=key, value=value, last_month=last_month, 
+            sender_name=sender_name, sender_phone=sender_phone, 
+            current_year=current_year)
+
+    funcs.create_pdf_from_list(value)
     email_body = content
     email = classes.EmailSender(email_sender, email_password, smtp_server)
     email.create_message(email_receiver, email_subject, email_body)
     email.attach_file(filename)
     email.send_email()
-
+    time.sleep(35)
